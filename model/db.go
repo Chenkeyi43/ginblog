d6680dd278f4e2709a049c536ef02c473296573c
sicheng43 <9121025+sicheng43@user.noreply.gitee.com>
2023-08-01 17:17:18 +0800

    add model

---
+++ model/db.go
@@ -0,0 +1,28 @@
+package model
+
+// 初始化数据库
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"os"
+)
+
+func InitDb() {
+	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
+	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DSN:                       "gorm:gorm@tcp(127.0.0.1:3306)/gorm?charset=utf8&parseTime=True&loc=Local", // DSN data source name
+		DefaultStringSize:         256,                                                                        // string 类型字段的默认长度
+		DisableDatetimePrecision:  true,                                                                       // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
+		DontSupportRenameIndex:    true,                                                                       // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
+		DontSupportRenameColumn:   true,                                                                       // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
+		SkipInitializeWithVersion: false,                                                                      // 根据当前 MySQL 版本自动配置
+	}), &gorm.Config{})
+	if err != nil {
+		fmt.Println("数据库连接失败，请检查参数")
+		os.Exit(1)
+	}
+
+}
