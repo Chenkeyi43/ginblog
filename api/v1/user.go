f666afe9e9df0282001687aaa204815961377cd6
sicheng43 <9121025+sicheng43@user.noreply.gitee.com>
2023-08-09 14:39:49 +0800

    添加编辑用户，删除用户，修改密码接口

--- api/v1/user.go
+++ api/v1/user.go
@@ -8,11 +8,6 @@ import (
 	"strconv"
 )
 
-// 查询用户是否存在
-func GetUser(c *gin.Context) {
-
-}
-
 // 添加用户
 func AddUser(c *gin.Context) {
 	//	todo 添加用户
@@ -31,6 +26,20 @@ func AddUser(c *gin.Context) {
 }
 
 // 查询单个用户
+func GetUserInfo(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	var maps = make(map[string]interface{})
+	data, code := model.GetUser(id)
+	maps["username"] = data.Username
+	maps["role"] = data.Role
+	c.JSON(
+		http.StatusOK, gin.H{
+			"status":  code,
+			"data":    data,
+			"total":   1,
+			"message": errmsg.GetErrMsg(code),
+		})
+}
 
 // 查询用户列表
 func GetUsers(c *gin.Context) {
@@ -62,10 +71,39 @@ func GetUsers(c *gin.Context) {
 
 // 编辑用户
 func EditUser(c *gin.Context) {
+	var data model.User
+	// 获取url 参数
+	id, _ := strconv.Atoi(c.Param("id"))
+	// 绑定body 到结构体
+	_ = c.ShouldBindJSON(&data)
+	code := model.CheckUpUser(id, data.Username)
+	if code == errmsg.SUCCSE {
+		model.EditUser(id, &data)
+	}
 
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
+
+// 修改用户密码
+func ChangeUserPassword(c *gin.Context) {
+	var data model.User
+	id, _ := strconv.Atoi(c.Param("id"))
+	code := model.ChangePassword(id, &data)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
 
 // 删除用户
 func DelUser(c *gin.Context) {
-
+	id, _ := strconv.Atoi(c.Param("id"))
+	code := model.DeleteUser(id)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
--- routes/router.go
+++ routes/router.go
@@ -16,6 +16,8 @@ func InitRouter() {
 		auth.GET("admin/users", v1.GetUsers)
 		auth.PUT("user/:id", v1.EditUser)
 		auth.DELETE("user/:id", v1.DelUser)
+		// 修改密码
+		auth.PUT("admin/changepw/:id", v1.ChangeUserPassword)
 		//	分类模块的路由接口
 
 		//	文章模块的路由接口
@@ -23,6 +25,8 @@ func InitRouter() {
 	route := r.Group("api/v1")
 	{
 		route.POST("user/add", v1.AddUser)
+		route.GET("user/:id", v1.GetUserInfo)
+		route.GET("users", v1.GetUsers)
 	}
 	// 定义一个没有默认中间件的路由
 	//r := gin.New()
